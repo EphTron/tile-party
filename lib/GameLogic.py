8f35ccefed4ca3911bc89de3ba411db66f32561e
EphTron <es.90@gmx.de>
2017-05-31 01:38:50 +0200

    added a simple event buffer and functions to update and wait for events

--- lib/GameLogic.py
+++ lib/GameLogic.py
@@ -5,8 +5,12 @@ Created on May 23.05.17 15:26
 @author: ephtron
 """
 
+import logging
+
 from lib.Player import Player
 from lib.Level import Level
+
+from tornado.concurrent import Future
 
 
 class GameLogic:
@@ -14,10 +18,65 @@ class GameLogic:
         """
         Constructor of the GameLogic 
         """
+        # Event variables
+        self.waiters = set()
+        self.event_cache = []  # Todo: make event_cache a dict {}
+        self.cache_size = 10
+
+        # Game variables and dictonaries
         self.players = {}
         self.levels = {}
 
+        # Init game
         self.create_level(4)
+
+    def wait_for_events(self, cursor=None):
+        """
+        Constructs a Future event to return to our caller.
+        :param cursor: int
+        :return: future event_obj
+        """
+        result_future = Future()
+        if cursor:
+            new_count = 0
+            for msg in reversed(self.event_cache):
+                if msg["id"] == cursor:
+                    break
+                new_count += 1
+            if new_count:
+                result_future.set_result(self.event_cache[-new_count:])
+                return result_future
+        self.waiters.add(result_future)
+
+        return result_future
+
+    def cancel_wait(self, future):
+        """
+        Set an empty result to unblock any coroutines waiting.
+        :param future: future event
+        :return: 
+        """
+        self.waiters.remove(future)
+        future.set_result([])
+
+    def new_event(self, event_list):
+        """
+        Creates new events and 
+        :param event: event_obj
+        :return: 
+        """
+        logging.info("Sending new message to %r listeners", len(self.waiters))
+
+        for future in self.waiters:
+            future.set_result(event_list)
+        self.waiters = set()
+        self.event_cache.extend(event_list)
+
+        # Todo: when event_cache is a dict - fill it
+        # event = event_list[0]
+        # self.event_cache[event['id']] = event_list
+        if len(self.event_cache) > self.cache_size:
+            self.event_cache = self.event_cache[-self.cache_size:]
 
     def create_player(self, player_name):
         """
@@ -26,6 +85,20 @@ class GameLogic:
         """
         _player = Player(player_name)
         self.players[_player.get_id()] = Player
+
+    def remove_player(self, player_name=None, player_id=None):
+        """
+        Removes a player from the player dictonary
+        :param player_name: str
+        :param player_id: id
+        :return: 
+        """
+        if player_id:
+            if player_id in self.players:
+                del self.player[player_id]
+        elif player_name:
+            if player_name in self.players.values():
+                pass
 
     def get_player(self, player_id):
         """
