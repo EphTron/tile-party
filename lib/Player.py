c8c7ac92f42767e4d47da646e584d37ebfb0c875
EphTron <es.90@gmx.de>
2017-06-08 00:03:45 +0200

    modified player class to not only hold the current tile id but the whole tile obj, adjusted the to_json function

--- lib/Player.py
+++ lib/Player.py
@@ -17,19 +17,23 @@ class Player:
         self.id = Player.instances
         Player.instances += 1
         self.name = player_name
-        self.current_map_id = None
+        self.current_tile = None
 
     def to_json(self):
         """
         Creates class specific json encode file
         :return: json format 
         """
-        json = {"id": self.id,
-                "name": self.name,
-                "current_map_id": self.current_map_id
-                }
-        print("json repr:", json)
-        return json
+        _json = {"id": self.id,
+                 "name": self.name
+                 }
+        if self.current_tile:
+            _json["current_tile_id"] = self.current_tile.get_id()
+        else:
+            _json["current_tile_id"] = None
+
+        print("json repr:", _json)
+        return _json
 
     def get_id(self):
         """
@@ -37,3 +41,18 @@ class Player:
         :return: int 
         """
         return self.id
+
+    def get_name(self):
+        """
+        Returns the name of this player
+        :return: str 
+        """
+        return self.name
+
+    def set_current_tile(self, tile_obj):
+        """
+        Set the current tile the player is on
+        :param tile_obj: 
+        """
+        # Todo: (Later Addon) add a check if the player was able to move onto this tile
+        self.current_tile = tile_obj
